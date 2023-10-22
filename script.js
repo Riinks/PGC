5e79d9ebc47da1fb786da09ef4f8980d6020f23e
Ringo <97435015+Riinks@users.noreply.github.com>
2023-10-22 22:33:24 +0300

    yuh

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('#slider-nav').addEventListener('click', (event) => {
+    const $slide = document.querySelector(event.target.getAttribute('href'));
+    if (!$slide) return;
+    
+    if ($slide.scrollIntoViewIfNeeded) {
+      event.preventDefault();
+      $slide.scrollIntoViewIfNeeded();
+    } else if ($slide.scrollIntoView) {
+      event.preventDefault();
+      $slide.scrollIntoView();
+    }
+  });
